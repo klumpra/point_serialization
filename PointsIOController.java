d009ad8c3697447bc285179b234feb3b652e3587
klumpra <raymond.klump@gmail.com>
2018-12-02 16:22:07 -0600

    Started implementing serialization.

--- PointsIOController.java
+++ PointsIOController.java
@@ -1,9 +1,22 @@
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
+@SuppressWarnings("unchecked")
 public class PointsIOController {
 	public boolean saveToBinary(ArrayList<Point> points, File f) {
-		return true;
+		try {
+			ObjectOutputStream oos = new ObjectOutputStream(
+					new FileOutputStream(f));
+			oos.writeObject(points);
+			oos.close();
+			return true;
+		} catch (Exception ex) {
+			return false;
+		}
 	}
 	public boolean saveToXML(ArrayList<Point> points, File f) {
 		return true;
@@ -12,7 +25,15 @@ public class PointsIOController {
 		return true;
 	}
 	public ArrayList<Point> readFromBinary(File f) {
-		return null;
+		try {
+			ArrayList<Point> results = new ArrayList<Point>();
+			ObjectInputStream ois = new ObjectInputStream( 
+					new FileInputStream(f));
+			results = (ArrayList<Point>)(ois.readObject());
+			return results;
+		} catch (Exception ex) {
+			return null;
+		}
 	}
 	public ArrayList<Point> readFromXML(File f) {
 		return null;
